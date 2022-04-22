4a6f725a360954f0a196eb57b7d3782048f2ed4b
Criste Phillips <criste.phillips@yahoo.com>
2022-04-22 14:38:53 -0400

    mutations updated

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,63 @@
+// template; update with current schema
+
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(bookData: bookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks{ 
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook(bookID: $bookID) {
+    removeBook(bookID: $bookID) {
+      _id
+      username
+      email
+      savedBooks{ 
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
