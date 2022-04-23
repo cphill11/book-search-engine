9cede7757237f3548b5b06d096896a0026d8b0c0
Criste Phillips <criste.phillips@yahoo.com>
2022-04-23 12:18:46 -0400

    resolver pull method updated

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -58,7 +58,7 @@ const resolvers = {
       if (context.user) {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
-          { $pull: { savedBooks: { _id: bookID } } },
+          { $pull: { savedBooks: { bookId: bookID } } },
           { new: true }
         );
         return updatedUser;
